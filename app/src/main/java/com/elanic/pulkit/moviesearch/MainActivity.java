966c2d473e9be544fbe976f5dcf5d481075cc9cf
pkarira <pulkit5208@gmail.com>
2017-03-26 19:59:44 +0530

    api calls implemented with interface

--- app/src/main/java/com/elanic/pulkit/moviesearch/MainActivity.java
+++ app/src/main/java/com/elanic/pulkit/moviesearch/MainActivity.java
@@ -2,12 +2,35 @@ package com.elanic.pulkit.moviesearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    String type="movie";
+    String movie="Sultan";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        omdbapi.Factory.getInstance().getInfo(movie,type).enqueue(new Callback<Movies>() {
+
+            @Override
+            public void onResponse(Call<Movies> call, Response<Movies> response) {
+
+                if (response.body().getSearch() != null)
+                {
+                    Toast.makeText(getApplicationContext(),response.body().getSearch()[0]+"",Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<Movies> call, Throwable t) {
+                Log.e("failed", t.getMessage());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/elanic/pulkit/moviesearch/omdbapi.java
@@ -0,0 +1,30 @@
+package com.elanic.pulkit.moviesearch;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by pulkit on 26/3/17.
+ */
+
+public interface omdbapi {
+    String  baseurl="http://www.omdbapi.com";
+    @GET("/")
+    Call<Movies> getInfo(@Query("s")String apiKey, @Query("type")String email);
+    class Factory
+    {
+        public static omdbapi services=null;
+        public static omdbapi getInstance() {
+            if (services == null) {
+                Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl(baseurl).build();
+                services = retrofit.create(omdbapi.class);
+                return services;
+            }else
+            {
+                return services;
+            }
+        }}
+}
